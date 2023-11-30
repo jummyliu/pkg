acb9b8b029b4f27235c60d738f8bab6fb0d4556a
jummyiu <jummyliu@foxmail.com>
2023-11-30 18:21:09 +0800

    [+] request 包增加自定义 client 和 proxy 功能

--- request/request.go
+++ request/request.go
@@ -5,8 +5,9 @@ import (
 	"context"
 	"crypto/tls"
 	"errors"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"net/url"
 	"time"
 	// "go.elastic.co/apm"
 	// "go.elastic.co/apm/module/apmhttp"
@@ -20,6 +21,8 @@ type Options struct {
 	timeout int
 	ssl     bool
 	ctx     context.Context
+	client  *http.Client
+	proxy   func(*http.Request) (*url.URL, error)
 }
 
 // Option request option
@@ -88,6 +91,47 @@ func WithContext(ctx context.Context) Option {
 	}
 }
 
+// WithClient use custom http.Client
+//
+//	client will invalidate WithSSL, WithTimeout, WithProxy
+func WithClient(client *http.Client) Option {
+	return func(opts *Options) {
+		opts.client = client
+	}
+}
+
+// WithProxy specifies a function to return a proxy for a given
+// Request. If the function returns a non-nil error, the
+// request is aborted with the provided error.
+//
+// The proxy type is determined by the URL scheme. "http",
+// "https", and "socks5" are supported. If the scheme is empty,
+// "http" is assumed.
+//
+// If Proxy is nil or returns a nil *URL, no proxy is used.
+func WithProxy(proxy string) Option {
+	return func(opts *Options) {
+		opts.proxy = func(r *http.Request) (*url.URL, error) {
+			return url.Parse(proxy)
+		}
+	}
+}
+
+// WithProxyFn specifies a function to return a proxy for a given
+// Request. If the function returns a non-nil error, the
+// request is aborted with the provided error.
+//
+// The proxy type is determined by the URL scheme. "http",
+// "https", and "socks5" are supported. If the scheme is empty,
+// "http" is assumed.
+//
+// If Proxy is nil or returns a nil *URL, no proxy is used.
+func WithProxyFn(fn func(*http.Request) (*url.URL, error)) Option {
+	return func(opts *Options) {
+		opts.proxy = fn
+	}
+}
+
 // DoRequest exec https? request and return []byte
 func DoRequest(url string, options ...Option) (code int, respBuf []byte, respHeader map[string][]string, err error) {
 	// exec the undercourse request
@@ -96,7 +140,7 @@ func DoRequest(url string, options ...Option) (code int, respBuf []byte, respHea
 		// 错误
 		return -1, nil, nil, errors.New("response failure")
 	}
-	respBuf, err = ioutil.ReadAll(resp.Body)
+	respBuf, err = io.ReadAll(resp.Body)
 	if err != nil {
 		return resp.StatusCode, nil, resp.Header, errors.New("read response failure")
 	}
@@ -126,17 +170,21 @@ func DoRequestUndercourse(url string, options ...Option) (resp *http.Response, e
 	for key, val := range opts.headers {
 		req.Header.Set(key, val)
 	}
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: opts.ssl,
+	client := opts.client
+	if opts.client == nil {
+		client = &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					InsecureSkipVerify: opts.ssl,
+				},
+				DisableKeepAlives: true,
+				Proxy:             opts.proxy,
 			},
-			DisableKeepAlives: true,
-		},
-		Timeout: time.Duration(opts.timeout) * time.Second,
-		// CheckRedirect: func(req *http.Request, via []*http.Request) error {
-		// 	return http.ErrUseLastResponse
-		// },
+			Timeout: time.Duration(opts.timeout) * time.Second,
+			// CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			// 	return http.ErrUseLastResponse
+			// },
+		}
 	}
 	// client = apmhttp.WrapClient(client)
 	// resp, err = client.Do(req.WithContext(ctx))
