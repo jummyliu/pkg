3319711619f3e803659b4f73b999bc9b2e205395
jummyiu <jummyliu@foxmail.com>
2024-06-26 09:55:20 +0800

    移除 mysqlbuilder.Map

--- db/mysqlbuilder/struct.go
+++ db/mysqlbuilder/struct.go
@@ -1,25 +1,5 @@
 package mysqlbuilder
 
-import (
-	"encoding/json"
-	"errors"
-)
-
-type Map map[string]any
-
-// Scan 实现 Scan 接口，用来自动赋值
-func (m *Map) Scan(src any) error {
-	if src == nil {
-		return nil
-	}
-	if src, ok := src.([]byte); ok {
-		if err := json.Unmarshal(src, m); err != nil {
-			return nil
-		}
-	}
-	return errors.New("try scan to Map failure")
-}
-
 type Count struct {
 	Count int64 `db:"count"`
 }
